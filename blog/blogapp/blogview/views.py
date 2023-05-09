58f24626fb1248edbfc237f3d6ac62069e599969
Krim-code <suhetsky.gricha555@gmail.com>
2023-05-09 20:25:36 +0300

    add static , templates and other stuff

--- blog/blogapp/blogview/views.py
+++ blog/blogapp/blogview/views.py
@@ -1,9 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+posts = [
+    {
+        'title': 'Beautiful is better than ugly',
+        'author': 'John Doe',
+        'content': 'Beautiful is better than ugly',
+        'published_at': 'October 1, 2022'
+    },
+    {
+        'title': 'Explicit is better than implicit',
+        'author': 'Jane Doe',
+        'content': 'Explicit is better than implicit',
+        'published_at': 'October 1, 2022'
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request,'blog/home.html')
+    context = {
+        'posts':posts,
+        'title':"Posts"
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
     return  render(request,'blog/about.html')
