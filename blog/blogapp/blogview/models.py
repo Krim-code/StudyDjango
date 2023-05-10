7a9968ab547dad2b7e56d8528640c996dc6c1133
Krim-code <suhetsky.gricha555@gmail.com>
2023-05-10 09:05:29 +0300

    create simple model and also create migrations

--- blog/blogapp/blogview/models.py
+++ blog/blogapp/blogview/models.py
@@ -1,3 +1,9 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title =  models.CharField(max_length=120)
+    content = models.TextField()
+    published_at = models.DateTimeField(default=timezone.now)
